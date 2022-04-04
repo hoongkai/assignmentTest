7338cbe0ccba01c7953590288a41ce0d2df66e91
Hoong Kai Chan <hkchan.fl@gmail.com>
2022-04-04 20:57:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+custombucket = "desmondchongsoonchuen-employee"
+customregion = "us-east-1"
+customhost = "employee.cpz7zg34rcbs.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "dcsc12345"
+customdb = "employee"
+
+access_key = 'ASIA23IK2VOCHPG7EUM7'
+secret_access_key = 'Ttk8o38AQQ5vaIpXkIEYlUkJAuCnPuotfdq9ngSv'
+session_token = 'FwoGZXIvYXdzEL3//////////wEaDNIWN6vNQ4XisHo0OyLUARdxE2NKY5Dlgv7Q2BdTgKlp7MwfGe6qfOes5nNHnfp6X6BYX6wnXlLnT3xXC6GVFZwmp4x+RTAX7VCfbBeNkztjUQsw9DkRg3mzQuqaj4GEGoKwrJVv9mpt+mgeKlbw3MCpTcIhWIssLFQkwY8DH9taWPpHH8bt8du5zfumNdUw7OGQayCrvdXZjE2mc2rcNXRBp1xJ7owxHrqhXpujxvBxJMK41F1QF7hrVR+kVmTMwmqoumXtZfjeK3Ta9hDh2kvm+vE2bnH0lWbgHIxHsvV3c9vsKIXloJIGMi3zgcU55/k7vSfdp7XiesWMmLxKICxyOoQPXqtsxXPduPYpQ4zHpscYtYQwNN4='
